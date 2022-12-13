26d684ac257eea544f02887b00c9c4fb546942c7
Alan Kuriakose <aln0071@gmail.com>
2022-12-12 18:54:06 -0800

    [authentication]: authentication service base code

--- microservices/authentication/src/index.js
+++ microservices/authentication/src/index.js
@@ -2,10 +2,46 @@ const express = require('express')
 const app = express();
 const port = 3000;
 
+const users = []
+
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const secret = "some secret";
+
+app.use(express.json())
+
 app.get('/api/v1/authentication', (req, res) => {
     res.json({
         msg: "authentication"
     })
 })
+
+app.post('/api/v1/authentication/register', async (req, res) => {
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    users.push({ username, password: hashedPassword})
+    res.status(201).send(users);
+})
+
+app.post('/api/v1/authentication/login', async (req, res) => {
+    const { username, password } = req.body;
+    const user = users.find(u => u.username === username);
+    console.log(user)
+    if(user === undefined) {
+        return res.status(404).send("User does not exist!");
+    }
+    if(await bcrypt.compare(password, user.password)) {
+        const accessToken = generateAccessToken(user);
+        res.json({
+            accessToken
+        })
+    } else {
+        res.status(401).send("Password Incorrect!");
+    }
+})
+
+function generateAccessToken(user) {
+    return jwt.sign(user, secret, {expiresIn: "15m"})
+}
 
 app.listen(port, () => console.log(`[authentication] listening on port ${port}`))
