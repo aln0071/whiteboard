dd6b32236bb16b41d402db067ae65578451511c8
Snichat97 <34672064+Snichat97@users.noreply.github.com>
2023-04-11 15:07:18 -0700

    merge conflicts.

--- microservices/frontend/src/components/private-dashboard/profile/index.jsx
+++ microservices/frontend/src/components/private-dashboard/profile/index.jsx
@@ -90,81 +90,79 @@ function Profile(props) {
         });
     }
   }
-};
 
-
-return (
-  profileData != {} && (
-    <div>
-      <Form onSubmit={submitHandler}>
-        <Row>
-          <Col className="profile-left-pane">
-            <img
-              src={profileData.image}
-              style={{ width: "200px", height: "200px" }}
-            ></img>
-            <Form.Group controlId="formFileSm" className="mb-3">
-              <Form.Control type="file" size="sm" />
-            </Form.Group>
-          </Col>
-          <Col>
-            <Card style={{ width: "25rem" }} className="profile-right-pane">
-              <Form.Group className="mb-3" controlId="formBasicName">
-                <Form.Label>ID</Form.Label>
-                <Form.Control
-                  type="text"
-                  name="id"
-                  defaultValue={profileData._id}
-                  disabled
-                />
+  return (
+    profileData != {} && (
+      <div>
+        <Form onSubmit={submitHandler}>
+          <Row>
+            <Col className="profile-left-pane">
+              <img
+                src={profileData.image}
+                style={{ width: "200px", height: "200px" }}
+              ></img>
+              <Form.Group controlId="formFileSm" className="mb-3">
+                <Form.Control type="file" size="sm" />
               </Form.Group>
+            </Col>
+            <Col>
+              <Card style={{ width: "25rem" }} className="profile-right-pane">
+                <Form.Group className="mb-3" controlId="formBasicName">
+                  <Form.Label>ID</Form.Label>
+                  <Form.Control
+                    type="text"
+                    name="id"
+                    defaultValue={profileData._id}
+                    disabled
+                  />
+                </Form.Group>
 
-              <Form.Group className="mb-3" controlId="formBasicUsername">
-                <Form.Label>User Name</Form.Label>
-                <Form.Control
-                  type="text"
-                  name="username"
-                  defaultValue={profileData.username}
-                  disabled
-                />
-              </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicUsername">
+                  <Form.Label>User Name</Form.Label>
+                  <Form.Control
+                    type="text"
+                    name="username"
+                    defaultValue={profileData.username}
+                    disabled
+                  />
+                </Form.Group>
 
-              <Form.Group className="mb-3" controlId="formBasicRole">
-                <Form.Label>Role</Form.Label>
-                <Form.Control
-                  type="text"
-                  name="role"
-                  defaultValue={profileData.roles}
-                  disabled
-                />
-              </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicRole">
+                  <Form.Label>Role</Form.Label>
+                  <Form.Control
+                    type="text"
+                    name="role"
+                    defaultValue={profileData.roles}
+                    disabled
+                  />
+                </Form.Group>
 
-              <Form.Group className="mb-3" controlId="formBasicFirstName">
-                <Form.Label>First Name</Form.Label>
-                <Form.Control
-                  type="text"
-                  name="firstname"
-                  defaultValue={profileData.firstName}
-                />
-              </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicFirstName">
+                  <Form.Label>First Name</Form.Label>
+                  <Form.Control
+                    type="text"
+                    name="firstname"
+                    defaultValue={profileData.firstName}
+                  />
+                </Form.Group>
 
-              <Form.Group className="mb-3" controlId="formBasicLastName">
-                <Form.Label>Last Name</Form.Label>
-                <Form.Control type="text" name="lastname" defaultValue={profileData.lastName} />
-              </Form.Group>
-            </Card>
-          </Col>
-          <Button variant="primary" type="submit" className="button-save-changes">
-            Save Changes
+                <Form.Group className="mb-3" controlId="formBasicLastName">
+                  <Form.Label>Last Name</Form.Label>
+                  <Form.Control type="text" name="lastname" defaultValue={profileData.lastName} />
+                </Form.Group>
+              </Card>
+            </Col>
+            <Button variant="primary" type="submit" className="button-save-changes">
+              Save Changes
           </Button>
-          <Button onClick={() => setShowAnswerResponses(true)} className="button-save-changes">
-            Reset Password
+            <Button onClick={() => setShowAnswerResponses(true)} className="button-save-changes">
+              Reset Password
         </Button>
-        </Row>
-        <PasswordReset username={profileData.username} isOpen={showAnswerResponses} closeModal={() => setShowAnswerResponses(false)} />
-      </Form>
-    </div >)
-);
+          </Row>
+          <PasswordReset username={profileData.username} isOpen={showAnswerResponses} closeModal={() => setShowAnswerResponses(false)} />
+        </Form>
+      </div >)
+  );
 }
 
 export default Profile;
