3068bf44f868fab31e95dfaf2cda1f9c46bf8648
Alan Kuriakose <aln0071@gmail.com>
2023-04-10 20:29:36 -0700

    feature/dashboard: update css for login and register pages

--- microservices/frontend/src/components/public-dashboard/index.jsx
+++ microservices/frontend/src/components/public-dashboard/index.jsx
@@ -1,36 +1,29 @@
-
 import React from "react";
-import Button from 'react-bootstrap/Button';
-import Tab from 'react-bootstrap/Tab';
-import Tabs from 'react-bootstrap/Tabs';
-import { useNavigate } from 'react-router-dom'
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
 import Login from "../login";
 import Register from "../register";
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import "../../App.css"
 
 export default function PublicDashboard() {
-    const navigate = useNavigate();
-    return <div>
-        <Row>
-            <Col lg={10}>
-                <img src="backgroundWhiteboard.png" style={{ height: "100vh", width: "100%" }} />
-            </Col>
-            <Col lg={2}>
-                <Tabs
-                    defaultActiveKey="profile"
-                    id="uncontrolled-tab-example"
-                    className="mb-3 tabsCustom"
-                >
-                    <Tab eventKey="login" title="Login">
-                        <Login />
-                    </Tab>
-                    <Tab eventKey="register" title="Register">
-                        <Register />
-                    </Tab>
-                </Tabs>
-            </Col>
-        </Row>
+  return (
+    <div className="public-dashboard-container">
+      <div className="public-dashboard-background">
+        <img src="backgroundWhiteboard.png" />
+      </div>
+      <div className="login-and-register-forms">
+        <Tabs
+          defaultActiveKey="login"
+          id="uncontrolled-tab-example"
+          className="mb-3 tabsCustom"
+        >
+          <Tab eventKey="login" title="Login">
+            <Login />
+          </Tab>
+          <Tab eventKey="register" title="Register">
+            <Register />
+          </Tab>
+        </Tabs>
+      </div>
     </div>
+  );
 }
